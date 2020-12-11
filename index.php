8ffef9a7a154f06ec10fae0627d793ff15581b13
jarga99 <wiradika.den88@gmail.com>
2020-12-11 10:45:58 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,179 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&family=Raleway:wght@200;300;400&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Viga&display=swap" rel="stylesheet">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <!-- Import Style Manual -->
+    <link rel="stylesheet" href="css/style.css">
+
+    <title>SMP N 58 SURABAYA</title>
+</head>
+
+<!-- Area Header -->
+<div class="header">
+    <nav class="navbar navbar-expand-lg navbar-light ">
+        <div class="container">
+            <a class="navbar-brand " href="#">
+                <img src="img/smp.png" width="40" height="40" alt="">
+                <span>smp n 58 surabaya</span>
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav ml-auto">
+                    <a class="nav-item nav-link active " href="#"> Home <span class="sr-only">(current)</span></a>
+                    <a class="nav-item nav-link" href="#">Profil</a>
+                    <a class="nav-item nav-link" href="#">Berita</a>
+                    <a class="btn btn-primary tombol" href="#"><span>Login</span></a>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</div>
+<!-- Akhir Area Header -->
+
+<!-- Area Jumbotron -->
+
+<div class="jumbotron jumbotron-fluid">
+    <div class="container">
+        <h3 class="display-4">mudahkan aktifitasmu <span>belajarmu</span> menggunakan <span>teknologi</span></h3>
+        <div class="tbl">
+            <a class="btn btn-primary tombol" href="#"> >>> go to e-learning <<< </a> </div> </div> </div> <!-- Akhiran Jumbotron -->
+
+                    <!-- garis gird -->
+                    <div class="gird"></div>
+                    <!-- akhir dird -->
+
+                    <!-- Card Area -->
+                    <div class="container">
+                        <div class="row">
+                            <div class="card text-white bg-primary mb-3" style="max-width: 22rem;">
+                                <div class="card-header">Visi</div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Primary card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ad voluptas beatae molestiae a dignissimos, odio nesciunt similique. Explicabo non suscipit pariatur. Officia impedit nobis labore!.</p>
+                                </div>
+                            </div>
+
+                            <div class="card text-white bg-success mb-3" style="max-width: 22rem;">
+                                <div class="card-header">Misi</div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Success card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quidem in quas tenetur consequuntur facere vitae dolores autem illo! Autem exercitationem rerum quod, voluptatem enim veniam. </p>
+                                </div>
+                            </div>
+
+                            <div class="card text-white bg-warning mb-3" style="max-width: 22rem;">
+                                <div class="card-header">Prestasi</div>
+                                <div class="card-body">
+                                    <h5 class="card-title">Warning card title</h5>
+                                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, doloremque eius. Tempore minima quasi fugit. Eveniet perspiciatis, mollitia numquam distinctio reprehenderit aliquam ducimus provident ut vel quae nobis, commodi expedita. Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis, non temporibus officiis, quam eius odio vel provident enim tempora necessitatibus consequatur fugit voluptatem officia!
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Akhir card -->
+
+                    <!-- News Area -->
+                    <!-- <div class="news">
+                        <div class="container-fluid">
+                            <div class="card-deck">
+                                <div class="card">
+                                    <img class="card-img-top" src="..." alt="Card image cap">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <img class="card-img-top" src="..." alt="Card image cap">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div> -->
+                    <!-- Akhir News Area -->
+
+                    <!-- info -->
+                    <div class="info">
+                        <div class="container">
+                            <div class="row">
+                                <div class="info-header col">
+                                    <h2>tentang kami</h2>
+                                </div>
+                            </div>
+                            <div class="info-body row">
+                                <div class="imgInfo col-lg-6">
+                                    <img class="img-fluid" src="img/ze.png" alt="">
+                                </div>
+                                <div class="keterangan col-lg-5">
+                                    <h4>Raih mimpimu lewat mimpi</h4>
+                                    <p>Lorem ipsum dolor sit amet consectetur adipisicing
+                                        elit. Molestiae mollitia earum laudantium accusantium
+                                        reiciendis iure dicta fugit. Neque, magni necessitatibus.</p>
+                                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est, distinctio. Fuga explicabo consequatur dolore, aliquid sapiente sequi quidem exercitationem eveniet!</p>
+                                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. <a class="btn btn-info" href="#"> show more ... </a></p>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Akhir info -->
+
+                    <!-- Area Footer -->
+                    <div class="footer">
+                        <div class="container">
+                            <div class="list">
+                                <div class="row">
+                                    <div class="contact">
+                                        <p>Kontak Kami</p>
+                                        <ul>
+                                            <li>ajsffaf2@adfaj</li>
+                                            <li>asffaffea@jgam</li>
+                                        </ul>
+                                    </div>
+                                    <div class="about">
+                                        <p>Tentang</p>
+                                        <ul>
+                                            <li>hasdfejajsdfkjagh</li>
+                                            <li>hasdfguuaj3jadasdf</li>
+                                            <li>agsdfarfsdfhesg</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <p>Copyright@arga</p>
+                        </div>
+
+                    </div>
+                    <!-- Akhir Area Footer -->
+
+                    <body>
+
+                        <!-- Optional JavaScript -->
+                        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+                        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+                        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+                        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+                    </body>
+
+</html>
